ce65de84ae625cf48068d553967851d75a4bf2d6
bharathsapram <122095272+bharathsapram@users.noreply.github.com>
2023-03-12 18:09:04 +0530

    yes updated today

---
+++ final second practical/src/App.js
@@ -0,0 +1,10 @@
+import ImageView from './components/ImageComponent/ImageComponent';
+
+
+const App=()=> {
+  return (
+      <ImageView/>
+  );
+}
+
+export default App;
---
+++ final second practical/src/components/ImageComponent/ImageComponent.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import  classes from './ImageComponent.module.css';
+  
+const  ImageView=()=>{
+    const [file, setFile] = useState();
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+  
+    return (
+        <div className={classes.app}>
+            <h2>Add Image:</h2>
+
+            <input type="file" accept='image/*' onChange={handleChange} />
+            <img src={file} alt='' />
+
+  
+        </div>
+  
+    );
+}
+  
+export default ImageView;
