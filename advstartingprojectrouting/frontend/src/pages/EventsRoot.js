2ca5a03a207918f2b045c53f4205f663bea91a5e
bharathsapram <122095272+bharathsapram@users.noreply.github.com>
2023-03-13 19:42:19 +0530

    section 20 react

---
+++ advstartingprojectrouting/frontend/src/pages/EventsRoot.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import EventsNavigation from "../components/EventsNavigation";
+
+function EventsRootPage() {
+  return (
+    <>
+      <EventsNavigation />
+      <Outlet />
+    </>
+  );
+}
+export default EventsRootPage;
